59093eaee029aeef9a3708926d9dfd6b240c86c2
Miki18711 <mikolas.polasek@gmail.com>
2024-09-15 18:19:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function convertTemperature(tempCelsius, unit) {
     if (unit === 'F') {
         return (tempCelsius * 9/5 + 32);  
     }
-    return tempCelsius.toFixed(1);  
+    return tempCelsius;  
 }
 
 function convertWindSpeed(speedKmH, unit) {
