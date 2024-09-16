6bd9ffba8776189ddeb8996a347028df1837b6f1
Miki18711 <mikolas.polasek@gmail.com>
2024-09-16 16:32:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const apiKey = 0f95fd0f58a2451a95fd0f58a2a51a75;  
-const stationId = IPODLE19;  
+const apiKey = '0f95fd0f58a2451a95fd0f58a2a51a75';  
+const stationId = 'IPODLE19';  
 
 let tempUnit = 'metric';  
 let windUnit = 'metric';  
